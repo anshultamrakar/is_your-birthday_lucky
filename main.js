b459763b51cfda9c8b89391b6de50d34832b6b80
Anshul Tamrakar <anshultamrakar487@gmail.com>
2022-11-21 18:33:35 +0530

    style : added style and functionality

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-const btnCheck = document.querySelector("#btn-check")
+const birthDate = document.querySelector("#birth-date")
+const luckyNumber = document.querySelector("#lucky-number")
+const buttonCheck = document.querySelector("#btn-check")
+
+
+
+function checkBirthdayIsLucky(){
+    const dateBirth = birthDate.value
+    const sum = calculateSum(dateBirth)
+    console.log(sum)
+}
+
+
+function calculateSum(dateBirth){
+    dateBirth = dateBirth.replace("-" ,"")
+    
+}
+
+
+
+buttonCheck.addEventListener("click" , checkBirthdayIsLucky)
+
