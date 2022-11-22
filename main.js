fd9c89ad90a31ecd82dac9d54f441580bac82bd9
Anshul Tamrakar <anshultamrakar487@gmail.com>
2022-11-22 12:02:45 +0530

    style : added style to the app

--- main.js
+++ main.js
@@ -1,20 +1,10 @@
 const birthDate = document.querySelector("#birth-date")
 const luckyNumber = document.querySelector("#lucky-number")
 const buttonCheck = document.querySelector("#btn-check")
+const outputBox = document.querySelector("output-box")
 
 
 
-function checkBirthdayIsLucky(){
-    const dateBirth = birthDate.value
-    const sum = calculateSum(dateBirth)
-    console.log(sum)
-}
-
-
-function calculateSum(dateBirth){
-    dateBirth = dateBirth.replace("-" ,"")
-    
-}
 
 
 
